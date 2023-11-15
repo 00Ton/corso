a168c9ca2c2a09c96bd8a35596cf0b07826ab485
Antonio Grande <grande.antonio90@gmail.com>
2023-11-15 22:48:59 +0100

    Extra JS test

--- W4_D1/script.js
+++ W4_D1/script.js
@@ -49,6 +49,8 @@ ESERCIZIO 2
 
 // Esempio con Switch-Case True
 
+// const userSize = 16;
+
 // switch (true) {
 //   case userSize < 5:
 //     console.log("Tiny");
@@ -187,3 +189,89 @@ Crea un algoritmo che determini l'ammontare totale che deve essere addebitato al
 //   else if (i % 5 === 0) console.log(i + " Buzz");
 //   else console.log(i);
 // }
+
+// Esercizio 1: condizioni con ausilio di operatori logici
+// Maggiore e minore
+// Scrivi un programma che dati quattro numeri, restituisca in output il maggiore e il minore.
+
+// Esempio:
+// Input: a = 3, b = -1, c = 4, d = -2
+// Output: maggiore = 4, minore = -2
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// if (a > b && a > c && a > d) {
+//   console.log(a + " è il maggiore");
+// } else if (b > a && b > c && b > d) {
+//   console.log(d + " è il maggiore");
+// } else if (c > a && c > b && c > d) {
+//   console.log(c + " è il maggiore");
+// } else if (d > a && d > b && d > c) {
+//   console.log(d + " è il maggiore");
+// }
+
+// if (a < b && a < c && a < d) {
+//   console.log(a + " è il minore");
+// } else if (b < a && b < c && b < d) {
+//   console.log(d + " è il minore");
+// } else if (c < a && c < b && c < d) {
+//   console.log(c + " è il minore");
+// } else if (d < a && d < b && d < c) {
+//   console.log(d + " è il minore");
+// }
+
+// Esercizio 2: stringhe e numeri
+// Quanti anni ha?
+// Scrivi un programma che dato l’anno corrente e un anno di nascita determini:
+
+// l’età della persona
+// quanti anni sono necessari per raggiungere i 100
+// Esempio:
+// Input: anno corrente = 2021, anno di nascita = 1996
+// Output: età = 25, anni mancanti = 75
+
+// const currentYear = 2023;
+// const bornYear = 1990;
+// const age = currentYear - bornYear;
+// const toHundred = 100 - age;
+
+// console.log(age);
+// console.log(toHundred);
+
+// Esercizio 3: Ciclo for
+// Progetta un algoritmo in Javascript che stampi tutte le coppie di numeri naturali la cui somma è un numero intero a scelta.
+
+// Esempio
+// Input: numero 7
+// Output:
+// 0-7
+// 1-6
+// 2-5
+// 3-4
+// 4-3
+// 5-2
+// 6-1
+// 7-0
+
+// let sum = 8;
+// let num = sum;
+
+// for (let i = 0; i <= sum; i++) {
+//   console.log(num, i);
+//   num--;
+// }
+
+// Esercizio 4: metodo e proprietà
+// Il conta cifre
+// Scrivi un programma che dato un numero conti da quante cifre è formato.
+
+// Esempio
+// Input: numero: 245
+// Output: numero cifre: 3
+
+// let num = 2008;
+// let numDigit = num.toString();
+// console.log(numDigit.length);
